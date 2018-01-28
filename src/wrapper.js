72557f4cc2b8489dca480afbcfe293135208da5d
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 15:29:25 +0100

    Added wrapper for whole framework

---
+++ src/wrapper.js
@@ -0,0 +1,58 @@
+/*! Simple Script framework | BSD-3-Clause | https://github.com/Machy8/Grid */
+
+(function () {
+
+	'use strict';
+
+	// @import helpers/defineProperty.js
+	// @import helpers/immediateStatement.js
+	// @import helpers/isNodeListInstance.js
+	// @import helpers/isObject.js
+	// @import helpers/iterator.js
+	// @import helpers/originalStatement.js
+	// @import helpers/prototypesSet.js
+	// @import helpers/voidStatement.js
+
+	// @import components/addClass.js
+	// @import components/after.js
+	// @import components/append.js
+	// @import components/attr.js
+	// @import components/before.js
+	// @import components/children.js
+	// @import components/clone.js
+	// @import components/css.js
+	// @import components/data.js
+	// @import components/each.js
+	// @import components/eq.js
+	// @import components/find.js
+	// @import components/first.js
+	// @import components/hasClass.js
+	// @import components/height.js
+	// @import components/html.js
+	// @import components/innerHeight.js
+	// @import components/innerWidth.js
+	// @import components/is.js
+	// @import components/last.js
+	// @import components/next.js
+	// @import components/off.js
+	// @import components/offset.js
+	// @import components/on.js
+	// @import components/outerHeight.js
+	// @import components/outerWidth.js
+	// @import components/parent.js
+	// @import components/prepend.js
+	// @import components/prev.js
+	// @import components/prop.js
+	// @import components/ready.js
+	// @import components/remove.js
+	// @import components/removeAttr.js
+	// @import components/removeClass.js
+	// @import components/removeData.js
+	// @import components/text.js
+	// @import components/toggleClass.js
+	// @import components/val.js
+	// @import components/width.js
+	// @import components/wrap.js
+	// @import components/wrapInner.js
+
+})();
