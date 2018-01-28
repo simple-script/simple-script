ef230ab6a036b9c6374975d2d77e27904407d650
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 15:28:35 +0100

    Added originalStatement helper

---
+++ src/helpers/originalStatement.js
@@ -0,0 +1,24 @@
+/*
+ * This file is part of the Simple Script framework package.
+ *
+ * (c) Vladimír Macháček <8machy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+/* eslint-disable no-unused-vars */
+
+
+/**
+ * @param value
+ * @returns {{value: *, type: string}}
+ */
+function originalStatement(value)
+{
+	return {
+		value: value,
+		type: 'original'
+	}
+}
