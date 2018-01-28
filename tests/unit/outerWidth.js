0537659f1a18efb93033ba3a1328ffcde9fac33d
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 15:58:06 +0100

    Added outerWidth component test

---
+++ tests/unit/outerWidth.js
@@ -0,0 +1,29 @@
+/*
+ * This file is part of the Simple Script framework package.
+ *
+ * (c) Vladimír Macháček <8machy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+QUnit.test('Outer width', function (assert) {
+	var element = newElement('div').css({
+		width: '200px',
+		'border-left': '1px solid',
+		'border-right': '1px solid',
+		'padding-left': '10px',
+		'padding-right': '10px',
+		'margin-left': '10px',
+		'margin-right': '10px'
+	});
+
+	getWorkspace().html(element.outerHTML);
+	element = getWorkspace().find('div');
+
+	assert.equal(element.outerWidth(), 222);
+	element.width('100px');
+	assert.ok(element.outerWidth(), 122);
+	assert.ok(element.outerWidth(true), 142);
+});
