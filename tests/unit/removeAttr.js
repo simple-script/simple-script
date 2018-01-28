05d311b4b3d275a01b4f058145cc604e489608b0
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 15:59:23 +0100

    Added removeAttr component test

---
+++ tests/unit/removeAttr.js
@@ -0,0 +1,32 @@
+/*
+ * This file is part of the Simple Script framework package.
+ *
+ * (c) Vladimír Macháček <8machy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+QUnit.test('Remove attr', function (assert) {
+	var testDiv = newElement('div')
+		.attr({
+			id: "testDiv",
+			class: "classA classB"
+		})
+		.attr('data-number', 123);
+
+	getWorkspace().html(testDiv.outerHTML);
+
+	testDiv = document.find('#testDiv');
+
+	assert.ok(testDiv.hasClass('classA'));
+	assert.ok(testDiv.hasClass('classB'));
+	assert.equal(testDiv.attr('data-number'), 123);
+
+	testDiv.removeAttr('id class data-number');
+
+	assert.notOk(testDiv.attr('id'));
+	assert.notOk(testDiv.attr('class'));
+	assert.notOk(testDiv.attr('data-number'));
+});
