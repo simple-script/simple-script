dd80a73e1cb1e4063029e55e5a5a3fbdb15425d1
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 15:28:58 +0100

    Added voidStatement helper

---
+++ src/helpers/voidStatement.js
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the Simple Script framework package.
+ *
+ * (c) Vladimír Macháček <8machy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+/* eslint-disable no-unused-vars */
+
+
+/**
+ * @returns {{type: string}}
+ */
+function voidStatement()
+{
+	return {
+		type: 'void'
+	}
+}
