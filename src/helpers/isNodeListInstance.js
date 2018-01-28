15f29d369d405dce578987b8042847d90c449b0c
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 15:28:04 +0100

    Added isNodeListInstance helper

---
+++ src/helpers/isNodeListInstance.js
@@ -0,0 +1,21 @@
+/*
+ * This file is part of the Simple Script framework package.
+ *
+ * (c) Vladimír Macháček <8machy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+/* eslint-disable no-unused-vars */
+
+
+/**
+ * @param {*} value
+ * @returns {boolean}
+ */
+function isNodeListInstance(value)
+{
+	return value instanceof NodeList;
+}
