7391acca8e9ec22be39e1c7843c39ce07022d744
Vladimír Macháček <8machy@seznam.cz>
2018-01-28 16:00:11 +0100

    Added width component test

---
+++ tests/unit/width.js
@@ -0,0 +1,28 @@
+/*
+ * This file is part of the Simple Script framework package.
+ *
+ * (c) Vladimír Macháček <8machy@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+
+QUnit.test('Width', function (assert) {
+	var element = newElement('div').css({
+		width: '200px',
+		'border-left': '1px solid',
+		'border-right': '1px solid',
+		'padding-left': '10px',
+		'padding-right': '10px',
+		'margin-left': '10px',
+		'margin-right': '10px'
+	});
+
+	getWorkspace().html(element.outerHTML);
+	element = getWorkspace().find('div');
+
+	assert.equal(element.width(), 200);
+	element.width('100px');
+	assert.ok(element.width(), 100);
+});
